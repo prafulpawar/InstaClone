2a8bcae50b57eb8128b67b8d4ea2e75c02c48314
prafulpawar <prafuldukhi@gmail.com>
2025-02-15 22:28:24 +0530

    __Cors_Added

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,10 @@ const app = express()
 const userRoutes = require('../src/routes/user.routes')
 const postRoutes = require('../src/routes/post.routes')
 const indexRoutes = require('../src/routes/index.routes')
+const cors  = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({extend:true}))
-
+app.use(cors())
 app.use('/users',userRoutes)
 app.use('/posts',postRoutes)
 app.use('/',indexRoutes)
