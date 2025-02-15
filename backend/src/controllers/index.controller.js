da0e337617c2fe74e155251c88ce1328161dbfc5
prafulpawar <prafuldukhi@gmail.com>
2025-02-15 11:56:37 +0530

    __Feed_Index_Done

--- backend/src/controllers/index.controller.js
+++ backend/src/controllers/index.controller.js
@@ -1,6 +1,25 @@
+const postModel = require("../models/post.model")
+
 module.exports.feedController = async(req,res)=>{
     try{
-       
+       const feedData = await postModel.aggregate([
+        {
+            '$lookup': {
+                'from': 'users', 
+                'localField': '_id', 
+                'foreignField': 'posts', 
+                'as': 'author'
+            }
+        }, {
+            '$unwind': {
+                'path': '$author'
+            }
+        }
+    ])
+      return res.status(200).json({
+        data:feedData,
+        message:"SucessFully Fetched"
+      })
     }
     catch(error){
         return res.status(400).json({
