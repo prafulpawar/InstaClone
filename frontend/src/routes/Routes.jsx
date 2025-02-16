5723604fb42f9d9d29401c2445a969c462787ff5
prafulpawar <prafuldukhi@gmail.com>
2025-02-16 12:26:06 +0530

    __Chnages_Done

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Routes}  from 'react-router-dom'
 import Register from '../views/register/Register'
 import Login from '../views/login/Login'
+import Profile from '../views/profile/Profile'
 function AppRoutes() {
   return (
          <Router>
@@ -9,6 +10,7 @@ function AppRoutes() {
                <Route path='/' element = {<div> Home</div>}/>
                <Route path='/register' element = {<Register />}/>
                <Route path='/login' element = {<Login />}/>
+               <Route path='/profile' element= {<Profile/>}/>
             </Routes>
          </Router>
   )
---
+++ frontend/src/views/profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Profile() {
+  return (
+     <main>
+          <section>
+               
+          </section>
+     </main>
+  )
+}
+
+export default Profile
