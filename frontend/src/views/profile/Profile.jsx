3dd309989399448980fa94cc42b59c43959c035b
prafulpawar <prafuldukhi@gmail.com>
2025-02-16 19:06:13 +0530

    __Profile_Added

--- frontend/src/views/profile/Profile.jsx
+++ frontend/src/views/profile/Profile.jsx
@@ -7,11 +7,11 @@ function Profile() {
   return (
      <main>
           <section className='profile-section'>
-                 <div>
+                 <div className='container-div'>
                      <div className="top"> 
 
                         <div> 
-                            <img src="" alt="" />
+                            <img src="https://images.unsplash.com/photo-1739312023925-19eca8ca00aa?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                         </div>
                         <div>
                            <h1>Username</h1>
@@ -24,11 +24,13 @@ function Profile() {
                      <div className="bottom">
                            <div className="bottom-top">
                            <div> 
-                            <img src="" alt="" />
+                            <img src="https://media.istockphoto.com/id/181956971/photo/couple-in-a-beautiful-landscape.webp?a=1&s=612x612&w=0&k=20&c=gD-VKqNfqC9MTyGNK_CqQTcW_NV6tAS-V-OY7NuHUQA=" alt="" />
                         </div>
                         <div>
                            <h1>Username</h1>
                         </div>
+
+
                            </div>
                            <div className='posts'>
                               
