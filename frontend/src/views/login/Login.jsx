e2e689fe60ec357245cb566873ca6889c9db5913
prafulpawar <prafuldukhi@gmail.com>
2025-02-16 11:51:40 +0530

    __Added_Scc_Design_Done

--- frontend/src/views/login/Login.jsx
+++ frontend/src/views/login/Login.jsx
@@ -3,16 +3,18 @@ import './Login.css'
 
 
 function Login() {
+  
+
   return (
      <main>
          <section className='sections-login'>
                <div className='input-groups'>
-                      <label htmlFor="">Email</label>
+                      <label htmlFor="">Email :</label>
                       <input type="email" name="email" id="email"  placeholder='Enter Email'/>
                </div>
 
                <div className='input-groups'>
-                      <label htmlFor="">Password</label>
+                      <label htmlFor="">Password :</label>
                       <input type="password" name="password" id="=password"  placeholder='Enter Password'/>
                </div>
 
