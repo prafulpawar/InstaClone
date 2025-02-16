ca6e0ba0bab6e703dd4a652dcdd172330b42d390
prafulpawar <prafuldukhi@gmail.com>
2025-02-16 18:44:42 +0530

    __Profile_Added

--- frontend/src/views/profile/Profile.jsx
+++ frontend/src/views/profile/Profile.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Profile() {
+  const [username,setUsername] = useState("");
   return (
      <main>
           <section>
-               
+                 <div>
+                     <div className="top"> 
+                      
+                        <div> 
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                           <h1>Username</h1>
+                        </div>
+
+                     </div>
+
+
+
+                     <div className="bottom">
+                           <div className="bottom-top">
+                           <div> 
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                           <h1>Username</h1>
+                        </div>
+                           </div>
+                           <div className='posts'>
+                              
+                           </div>
+                     </div>
+
+
+                 </div>
           </section>
      </main>
   )
