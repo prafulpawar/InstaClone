4f654d7bea517fc5e77f63333b94882c63600818
prafulpawar <prafuldukhi@gmail.com>
2025-02-16 11:57:47 +0530

    __Login_Done

--- frontend/src/views/login/Login.jsx
+++ frontend/src/views/login/Login.jsx
@@ -1,37 +1,41 @@
 import React, { useState } from 'react'
 import './Login.css'
-
+import axios from 'axios'
 
 function Login() {
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("")
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("")
+    const handleClick = (e)=>{
+        e.preventDefault();
+        axios.post('')
 
-  return (
-     <main>
-         <section className='sections-login'>
-               <form action="">
-
-
-               <div className='input-groups'>
-                      <label htmlFor="">Email :</label>
-                      <input type="email" name="email" id="email"  placeholder='Enter Email'/>
-               </div>
-
-               <div className='input-groups'>
-                      <label htmlFor="">Password :</label>
-                      <input type="password" name="password" id="=password"  placeholder='Enter Password'/>
-               </div>
-
-               <button>
-                 Login
-               </button>
-
-               </form>
+    }
+    return (
+        <main>
+            <section className='sections-login'>
+                <form onSubmit={handleClick}>
 
 
-         </section>
-     </main>
-  )
+                    <div className='input-groups'>
+                        <label htmlFor="">Email :</label>
+                        <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" name="email" id="email" placeholder='Enter Email' />
+                    </div>
+
+                    <div className='input-groups'>
+                        <label htmlFor="">Password :</label>
+                        <input value={password} onChange={(e)=> setPassword(e.target.value)} type="password" name="password" id="=password" placeholder='Enter Password' />
+                    </div>
+
+                    <button>
+                        Login
+                    </button>
+
+                </form>
+
+
+            </section>
+        </main>
+    )
 }
 
 export default Login
