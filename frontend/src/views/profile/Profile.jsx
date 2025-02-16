ffce8a4139d2bbd1f667495909830b5f8739c33a
prafulpawar <prafuldukhi@gmail.com>
2025-02-16 19:24:36 +0530

    __Done__

--- frontend/src/views/profile/Profile.jsx
+++ frontend/src/views/profile/Profile.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
 import './Profile.css';
-
+import axios from 'axios'
+import { useEffect } from 'react';
 function Profile() {
-  const [username, setUsername] = useState("");
+
+  function getProfileData() {
+    axios.get('http://localhost:3000/users/profile', {
+      headers: {
+        authorization: "bearer" + localStorage.getItem('token')
+      }
+    }).then(res => {
+      console.log(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+
+  useEffect(() => {
+    getProfileData()
+  }, [])
+
 
   return (
     <main>
       <section className='profile-section'>
         <div className="top">
-          <img 
-            src="https://images.unsplash.com/photo-1739312023925-19eca8ca00aa?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          <img
+            src="https://images.unsplash.com/photo-1739312023925-19eca8ca00aa?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Profile"
           />
           <h1>Username</h1>
@@ -17,15 +35,15 @@ function Profile() {
 
         <div className="bottom">
           <div className="bottom-top">
-            <img 
-              src="https://media.istockphoto.com/id/181956971/photo/couple-in-a-beautiful-landscape.webp?a=1&s=612x612&w=0&k=20&c=gD-VKqNfqC9MTyGNK_CqQTcW_NV6tAS-V-OY7NuHUQA=" 
+            <img
+              src="https://media.istockphoto.com/id/181956971/photo/couple-in-a-beautiful-landscape.webp?a=1&s=612x612&w=0&k=20&c=gD-VKqNfqC9MTyGNK_CqQTcW_NV6tAS-V-OY7NuHUQA="
               alt="Profile Icon"
             />
             <h1>Username</h1>
           </div>
 
           <div className='posts'>
-              <img src="https://images.unsplash.com/photo-1739381337576-d14376e305d2?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img src="https://images.unsplash.com/photo-1739381337576-d14376e305d2?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
           </div>
         </div>
       </section>
