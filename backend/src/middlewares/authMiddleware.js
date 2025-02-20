ee97a824859405984faa1ea98dad41bbe23dcce2
prafulpawar <prafuldukhi@gmail.com>
2025-02-21 00:08:41 +0530

    __Done

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -5,7 +5,6 @@ module.exports.isAuth = async (req,res,next)=>{
          
          const token = req.headers.authorization ? req.headers.authorization.split(" ")[1] : null;
         
-        console.log(token)
         if(!token){
             
             return res.status(400).json({
