f3e8ccef3824af1e3b82144ee9b3b686e451f5bd
prafulpawar <prafuldukhi@gmail.com>
2025-02-21 11:28:58 +0530

    __Done__

---
+++ socket/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = require('./src/app')
+const htpp = require('http');
+
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection',client=>{
+    client.on('event', data => { /* _ */  })
+    client.on('disconnect', () =>{ /* _ */ })
+})
+
+server.listen(3000,()=>{
+    console.log(`Server Is Running On ${3000}`)
+})
